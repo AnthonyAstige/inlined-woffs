7ac6caf5360e8d983b6b2b3dbc4bdde0eb81ca26
Anthony At Zinid <anthony@zinid.com>
2017-03-01 01:15:09 -0600

    Progress: Sort by size, display more info, ...

--- src/local.babel.js
+++ src/local.babel.js
@@ -1,13 +1,20 @@
 $(document).ready(() => {
+	function linksRender(data, type, row) {
+		return `<a target="_blank" href="https://fonts.google.com/?query=${row.name}">Google Font</a>`
+	}
 	$('#myTable').DataTable({
 		data: _.map(fontsData, (dat) => {
 			dat.size /= 1024
 			return dat
 		}),
+		order: [
+			[3, 'asc']
+		],
 		columns: [
 			{ title: 'Font name', data: 'name' },
-			{ title: 'Weight/Style', data: 'weightStyle' },
-			{ title: 'Size [KB]', data: 'size' }
+			{ title: 'Weight/Style', data: 'weightStyle', defaultContent: '' },
+			{ title: 'Size [KB]', data: 'size', render: (size) => Math.round(size * 10) / 10 },
+			{ title: 'Links', render: linksRender, sortable: false }
 		]
 	})
 })
