b0ad6701ff69b37e010be614f24b4a6b2326bf8b
Anthony At Zinid <anthony@zinid.com>
2017-03-02 12:43:24 -0600

    Basic UI mock up, particularly custom subsets

--- src/js/app.babel.js
+++ src/js/app.babel.js
@@ -1,7 +1,81 @@
 /* global _, fontsData */
 $(document).ready(() => {
+	/**
+	 * Custom Subset
+	 */
+	function customSubset(event) {
+		const sets = [
+			{
+				title: 'A through Z (uppercase)',
+				glyphs: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+				inc: true
+			},
+			{
+				title: '" " (space)',
+				glyphs: ' ',
+				inc: true
+			},
+			{
+				title: 'a through z (lowercase)',
+				glyphs: 'abcdefghijklmnopqrstuvwxyz'
+			},
+			{
+				title: '0-9 (numbers)',
+				glyphs: '0123456789'
+			},
+			{
+				title: 'Special characters',
+				glyphs: '~`!@#$%^&*()-_=+{}[]\\|;:\'"<,>./?'
+			}
+		]
+
+		const glyphsSelected = sets
+			.filter((set) => set.inc)
+			.reduce((acc, set) => acc + set.glyphs.length, 0)
+		const fontName = 'abba'
+		let content = `<h2>\
+				Generate custom subset of ${fontName}\
+				(<span class="glyphs-selected">${glyphsSelected}</span> glyphs selected\
+			</h2>\
+			<button>Generate</button>`
+		content = sets.reduce((acc, set) => {
+			const glyphs = set.glyphs.split('').reduce((ac, glyph) =>
+				`${ac}<span class="tog${set.inc ? ' inc' : ''}">${glyph}</span> `, '')
+			return `<div>\
+					${acc} <h3 class="tog${set.inc ? ' inc' : ''}">${set.title}</h3>\
+					${glyphs}\
+				</div>`
+		}, content)
+
+		$('#customSubsetsDialog .custom-subset-dialog')
+			.html(content)
+		$('#customSubsetsDialog')
+			.show()
+
+		$('.custom-subset-dialog .tog').click((ev) => {
+			const target = ev.target
+			if ($(target).hasClass('inc')) {
+				$(target).removeClass('inc')
+			} else {
+				$(target).addClass('inc')
+			}
+			$('.custom-subset-dialog .glyphs-selected').html(
+				$('.custom-subset-dialog span.tog.inc').length
+			)
+		})
+	}
+
+	/**
+	 * DataTable
+	 */
 	function linksRender(data, type, row) {
-		return `<button ref="${row.name}">Generate</button>`
+		return '' +
+			`<button class="js-generate-full-subset" ref="${row.name}">\
+				Full Subset\
+			</button>\
+			<button class="js-generate-customer-subset" ref="${row.name}">\
+				Custom Subset\
+			</button> `
 	}
 	function preview(data, type, row) {
 		return 'ABCDE abcde'
@@ -9,7 +83,7 @@ $(document).ready(() => {
 	function kb(size) {
 		return Math.round(size * 10) / 10
 	}
-	$('#myTable').DataTable({ // eslint-disable-line new-cap
+	const table = $('#myTable').DataTable({ // eslint-disable-line new-cap
 		data: _.map(fontsData, (dat) => {
 			dat.size /= 1024
 			return dat
@@ -20,9 +94,17 @@ $(document).ready(() => {
 		columns: [
 			{ title: 'Derive from font', data: 'name' },
 			{ title: 'Weight/Style', data: 'weightStyle', defaultContent: '' },
-			{ title: 'Full Size [KB]', data: 'size', render: kb },
+			{ title: 'Full Subset Size [KB]', data: 'size', render: kb },
 			{ title: 'Preview', render: preview },
-			{ title: 'Links', render: linksRender, sortable: false }
+			{ title: 'Generate inlined font', render: linksRender, sortable: false }
 		]
+	})
+
+	/**
+	 * Connect DataTable to Custom Subset generaiton
+	 */
+	$('.js-generate-customer-subset').click(customSubset)
+	table.on('draw', () => {
+		$('.js-generate-customer-subset').click(customSubset)
 	})
 })
